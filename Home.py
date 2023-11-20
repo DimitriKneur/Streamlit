cdbdcfb940a7387c18a53f7757d86fc1934d5962
DimitriKneur <dimitri.kneur@gmail.com>
2023-11-20 03:22:27 +0100

    modified home page

--- Home.py
+++ Home.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Welcome to my dev. website !")
+
+st.text("Hello folks,")
+st.text("My name is Dimitri Kneur.")
+st.text("I try to learn more and more about the Python language, time after time.")
+st.text("You are currently into my development website, designed to host specifically apps and small programs I coded in Python.")
+st.text("I wish you a pleasant journey among the different webpages, I hope they will entertain you.")
+st.text("Regards,")
+st.text("Dimitri Kneur")
